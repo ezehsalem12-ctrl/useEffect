0e41c58f367a0dbe21aebf4535820516a649305a
Ezeh Salem <ezehsalem12@gmail.com>
2026-04-17 14:16:54 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Posts from "./component/Posts";
+
+export default function App() {
+  return (
+    <div>
+      <Posts />
+    </div>
+  );
+}
---
+++ src/component/Posts.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+type PostType = { id: number; title: string; body: string };
+
+export default function Posts() {
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+        const data = await res.json();
+
+        if (mounted) {
+          setPosts(data);
+        }
+      } catch (e) {
+        console.error(Error);
+      }
+    };
+
+    fetchPosts();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
